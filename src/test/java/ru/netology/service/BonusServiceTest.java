207ba5d7a68ef7dd0c5d15cf999bd97d7a6fa53d
Sergey K <89184083452.mail.ru>
2020-09-26 00:04:47 +0300

    Часть 1. TestNG

--- src/test/java/ru/netology/service/BonusServiceTest.java
+++ src/test/java/ru/netology/service/BonusServiceTest.java
@@ -11,14 +11,14 @@ class BonusServiceTest {
         BonusService bonusService = new BonusService();
         int expected = 5;
         int actual = bonusService.calculateBonus(1566);
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
     @Test
     void calculateBonus_20000() {
         BonusService bonusService = new BonusService();
         int expected = 100;
         int actual = bonusService.calculateBonus(20000);
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
 
 }
