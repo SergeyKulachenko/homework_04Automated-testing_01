3a2c0a0de2f0e08f526514cbaed3703f6dc2b1dd
Sergey K <89184083452.mail.ru>
2020-09-28 13:40:20 +0300

    удаление лишних классов

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,11 +7,27 @@ import static org.junit.jupiter.api.Assertions.*;
 class CashbackHackServiceTest {
 
     @Test
-    void remain() {
-        BonusService bonusService = new BonusService();
+    void remainAt1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int executed=0;
+        int actual = cashbackHackService.remain(1000);
+        assertEquals(executed,actual);
+    }
+
+    @Test
+    void remainUpTo1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int executed=1;
+        int actual = cashbackHackService.remain(999);
+        assertEquals(executed,actual);
+    }
+
+    @Test
+    void remainMove1000() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int executed=95;
         int actual = cashbackHackService.remain(1905);
         assertEquals(executed,actual);
     }
+
 }
