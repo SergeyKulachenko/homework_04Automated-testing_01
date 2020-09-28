932295fbc7d52f65b50bf3dc16695f900e35f3f7
Sergey K <89184083452.mail.ru>
2020-09-28 14:55:49 +0300

    исправления

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,17 +1,33 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.*;
 
-class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
 
-    @Test
-    void remain() {
-        BonusService bonusService = new BonusService();
+    @org.testng.annotations.Test
+    public void remainAt1000() {
         CashbackHackService cashbackHackService = new CashbackHackService();
-        int executed=95;
-        int actual = cashbackHackService.remain(1905);
-        assertEquals(executed,actual);
+        int expected = 0;
+        int actual = cashbackHackService.remain(1000);
+        assertEquals(actual,expected);
     }
+
+    @org.testng.annotations.Test
+    public void remainUpTo1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int expected = 1;
+        int actual = cashbackHackService.remain(999);
+        assertEquals(actual,expected);
+    }
+
+    @org.testng.annotations.Test
+    void remainMove1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int expected = 95;
+        int actual = cashbackHackService.remain(1905);
+        assertEquals(actual,expected);
+    }
+
 }
